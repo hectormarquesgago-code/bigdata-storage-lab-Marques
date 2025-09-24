31333850465bf28aa674627a8d05c00fb4b2065a
hectormarquesgago-code <hectormarquesgago@gmail.com>
2025-09-24 14:55:29 +0200

    Create validate.py

---
+++ src/validate.py
@@ -0,0 +1,8 @@
+"""
+Módulo: validate.py
+Objetivo: aplicar reglas de validación de calidad a los datasets.
+"""
+
+def validate_dataframe(df):
+    # TODO: implementar reglas de validación
+    return df
