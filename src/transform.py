4281e6e160527fbc8ac207c97b2dcaba2a27c352
hectormarquesgago-code <hectormarquesgago@gmail.com>
2025-09-24 14:55:54 +0200

    Create transform.py

---
+++ src/transform.py
@@ -0,0 +1,8 @@
+"""
+Módulo: transform.py
+Objetivo: normalizar, estandarizar y mover datos hacia capas Silver/Gold.
+"""
+
+def transform_dataframe(df):
+    # TODO: implementar transformaciones de normalización
+    return df
